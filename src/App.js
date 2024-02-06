8ccdfaef4665463e2ed0f6e32e4466284b89ec4d
vinhdn20 <vinhdn20@gmail.com>
2024-02-06 08:27:07 +0700

    add  route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
+import { Route, Routes, Router } from "react-router-dom";
 import SideBar from "./SideBar/JS";
+import Home from "./pages/Home";
 
 function App() {
-  return <SideBar />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+  // return <SideBar />;
 }
 
 export default App;
---
+++ src/Note/JS/index.js
@@ -0,0 +1,3 @@
+export default function Note() {
+  return <h1>New Note</h1>;
+}
